4bf1cab6aafec3c009b767445715199dfce8ac5b
bimal.poudel <bimal.poudel@fcd22dad-e209-4754-a333-3822e3321d1f>
2011-01-20 20:36:50 +0000

    Using match counter.

--- inc.config.php
+++ inc.config.php
@@ -5,6 +5,11 @@ header('Content-Type: text/plain');
 # Run infinitely: use either of the below:
 set_time_limit(0);
 ini_set('max_execution_time', '0');
+ini_set('max_input_time', '-1');
+ini_set('memory_limit', '500M');
+
+# Say everything. But be careful that we are running on an intensive loop!
+error_reporting(E_ALL|E_STRICT);
 
 # Even if the browser has closed, continue working.
 ignore_user_abort(true);
