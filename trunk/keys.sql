948bc1b4dc264659f84757ec39d9bdf6b0143e7c
bimal.poudel <bimal.poudel@fcd22dad-e209-4754-a333-3822e3321d1f>
2011-01-25 18:40:08 +0000

    relocating the files.

--- trunk/keys.sql
+++
@@ -1,5 +0,0 @@
-# Before INSERT: disable the keys.
-ALTER TABLE physical_files DROP KEY hash_md5_key;
-
-# For generating reports: create the keys again.
-ALTER TABLE physical_files ADD KEY hash_md5_key (hash_md5);
